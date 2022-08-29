4eea841dadb92695e584c2aaf29b45da65787c6f
ChoMihyang <chomh316@gmail.com>
2022-08-29 17:01:13 +0900

    [Express] Creating Middleware

--- src/server.js
+++ src/server.js
@@ -4,8 +4,12 @@ const PORT = 4000;
 
 const app = express();
 
+const gossipMiddleware = (req, res, next) => {
+    console.log(`Someone is going to: ${req.url}`);
+    next();
+}
 const handleHome = (req, res) => {
-    return res.send("I still love you.");
+    return res.send("I love middleware.");
 };
 
 const handleLogin = (req, res) => {
@@ -13,7 +17,7 @@ const handleLogin = (req, res) => {
 }
 
 // Route
-app.get("/", handleHome);
+app.get("/", gossipMiddleware, handleHome);
 app.get("/login", handleLogin);
 
 const handleListening = () => 
