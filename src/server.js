369ef1a686b7fa6fb3c0a4e3d46e819241ed57f0
ChoMihyang <chomh316@gmail.com>
2022-09-12 13:50:05 +0900

    [Express]Installing Morgan

--- src/server.js
+++ src/server.js
@@ -1,39 +1,23 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const logger = (req, res, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-}
-const privateMiddleware = (req, res, next) => {
-    const url = req.url;
-    if(url === "/protected"){
-        return res.send("<h1>Not Allowed</h1>")
-    }
-    console.log("Allowd, you may continue.");
-    next();
-}
-
-const handleHome = (req, res) => {
-    return res.send("I love middleware.");
+const home = (req, res) => {
+    return res.send("hello");
 };
 
-const handleLogin = (req, res) => {
-    return res.send("Login here.");
-}
-
-const handelPretected = (req, res) => {
-    return res.send("Welcome to the private logger");
+const login = (req, res) => {
+    return res.send("login");
 }
 
 // Route
 app.use(logger);
-app.use(privateMiddleware);
-app.get("/", handleHome);
-app.get("/protected", handelPretected);
+app.get("/", home);
+app.get("/login", login);
 
 const handleListening = () => 
     console.log(`Server listening on http://localhost:${PORT} 👋`);
