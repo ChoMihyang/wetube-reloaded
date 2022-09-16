e4b06524da700dcb1ee7c8fbf8053e4cb2c57249
ChoMihyang <chomh316@gmail.com>
2022-09-16 17:19:39 +0900

    [TEMPLATES] Conditionals

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,9 @@
-export const trending = (req, res) => res.render("home", {pageTitle : "Home"});
+const fakeUser = {
+    username: "MH",
+    loggedIn: true,
+};
+
+export const trending = (req, res) => res.render("home", {pageTitle : "Home", fakeUser: fakeUser});
 
 export const see = (req, res) => res.render("watch");
 
