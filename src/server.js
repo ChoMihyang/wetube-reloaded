0eb01ce110cc53b9519d44d8e83d8bfc156ac7c4
ChoMihyang <chomh316@gmail.com>
2022-09-20 17:53:13 +0900

    [DATABASE] Edit Video

--- src/server.js
+++ src/server.js
@@ -12,6 +12,7 @@ const logger = morgan("dev");
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views"); // views 파일 경로 재설정
 app.use(logger);
+app.use(express.urlencoded({ extended: true }));
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
